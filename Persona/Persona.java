0ba719d5b443c386d87d9c1df6ba482c009a7f40
Nachorov027 <iruizoriol@gmail.com>
2021-09-21 20:07:11 +0200

    Primer Comit

--- Persona/Persona.java
+++ Persona/Persona.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Persona {
-}
+package Persona;
+
+    public class Persona {
+        private String nombre;
+        private String apellidos;
+        private int dnisinletra;
+        private int edad;
+
+        public Persona() { /* compiled code */ }
+
+        public String getNombre() { return nombre; }
+
+        public String getApellidos() { return apellidos; }
+
+        public int getDnisinletra() { return dnisinletra;}
+
+        public int getEdad() {return edad;}
+
+        public void setNombre(String nom) { this.nombre =nom; }
+
+        public void setApellidos(String apel) { this.apellidos =apel; }
+
+        public void setDnisinletra(int dni) { this.dnisinletra =dni;}
+
+        public void setEdad(int ed) {this.edad =ed;}
+    }
