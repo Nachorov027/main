53e7049cb1f8d611bd8a6cb28e84c6d4956ac89d
Nachorov027 <iruizoriol@gmail.com>
2021-09-21 20:20:20 +0200

    Primer commit en develop

--- Persona/Persona.java
+++ Persona/Persona.java
@@ -7,20 +7,12 @@ package Persona;
         private int edad;
 
         public Persona() { /* compiled code */ }
-
         public String getNombre() { return nombre; }
-
         public String getApellidos() { return apellidos; }
-
         public int getDnisinletra() { return dnisinletra;}
-
         public int getEdad() {return edad;}
-
         public void setNombre(String nom) { this.nombre =nom; }
-
         public void setApellidos(String apel) { this.apellidos =apel; }
-
         public void setDnisinletra(int dni) { this.dnisinletra =dni;}
-
         public void setEdad(int ed) {this.edad =ed;}
     }
